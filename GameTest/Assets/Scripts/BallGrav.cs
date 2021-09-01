c7fafec2d87d5c7e1fdc7f28377de08aefd262d8
Kadgarr <55097790+Kadgarr@users.noreply.github.com>
2021-09-01 19:04:58 +0300

    FirstPush
    New project and scene.

---
+++ GameTest/Assets/Scripts/BallGrav.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGrav : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float GravityScale;
+    void Start()
+    {
+        //GetComponent<Rigidbody>(). = GravityScale;
+    }
+
+   
+}
