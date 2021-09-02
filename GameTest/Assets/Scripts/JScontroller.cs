2da5ccffc433d77c920233afc699f36bb3e2e192
Kadgarr <55097790+Kadgarr@users.noreply.github.com>
2021-09-02 18:15:05 +0300

    2.09
    Контроллер

---
+++ GameTest/Assets/Scripts/JScontroller.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class JScontroller : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    private Image joystickBG;
+
+    [SerializeField]
+    private Image joystick;
+    private Vector2 inputVector;
+    
+    public float moveSpeed;
+    
+
+    void Start()
+    {
+        joystickBG = GetComponent<Image>();
+        joystick = transform.GetChild(0).GetComponent<Image>();
+        
+    }
+    public virtual void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickBG.rectTransform, eventData.position, eventData.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / joystickBG.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / joystickBG.rectTransform.sizeDelta.y);
+
+            inputVector = new Vector2(pos.x * 2 - 1, pos.y * 2 - 1);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            joystick.rectTransform.anchoredPosition = new Vector2(inputVector.x * (joystickBG.rectTransform.sizeDelta.x / 2), inputVector.y * (joystickBG.rectTransform.sizeDelta.y / 2));
+        }
+
+
+    }
+
+    public virtual void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public virtual void OnPointerUp(PointerEventData eventData)
+    {
+        inputVector = Vector2.zero;
+        joystick.rectTransform.anchoredPosition = Vector2.zero;
+    }
+
+    public float Horizontal()
+    {
+        if (inputVector.x != 0) return inputVector.x;
+        else return Input.GetAxis("Horizontal");
+    }
+
+    public float Vertical()
+    {
+        if (inputVector.y != 0) return inputVector.y;
+        else return Input.GetAxis("Vertical");
+    }
+}
---
+++ GameTest/Assets/Scripts/PlatContr.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatContr : MonoBehaviour
+{
+    public float speedMove;
+
+
+    private float gravityForce;
+    private Vector3 moveVector;
+    private Vector3 pointVector;
+    private JScontroller pContr;
+    private CharacterController characterController;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        pContr = GameObject.FindGameObjectWithTag("Joystick").GetComponent<JScontroller>();
+        pointVector.x = 0;
+        pointVector.y = 2.22f;
+        pointVector.z = -10.64f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlatformMove();
+    }
+
+    //перемещение платформы
+    private void PlatformMove()
+    {
+        moveVector = Vector3.zero;
+        moveVector.x = pContr.Vertical()* speedMove;
+        moveVector.y = pContr.Horizontal() * speedMove;
+
+        characterController.transform.RotateAround(pointVector, moveVector, 100 * Time.deltaTime);
+    }
+}
